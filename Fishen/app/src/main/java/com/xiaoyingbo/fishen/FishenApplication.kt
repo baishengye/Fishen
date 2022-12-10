db51e42cf9503d035f137a9f6ea849034452b016
xiaoyingbo <amazong-xyb@qq.com>
2022-12-10 21:10:37 +0800

    初始化项目

---
+++ Fishen/app/src/main/java/com/xiaoyingbo/fishen/FishenApplication.kt
@@ -0,0 +1,13 @@
+package com.xiaoyingbo.fishen
+
+import android.app.Application
+
+/**
+ * 整个app的application*/
+class FishenApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
